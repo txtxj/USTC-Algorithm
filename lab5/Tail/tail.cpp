c2e4d3ab1c3638fd0cda64a14aba69aa069d5ec9
txtxj <1418373194@qq.com>
2022-05-26 00:46:28 +0800

    feat: finish big int

--- lab5/Tail/tail.cpp
+++ lab5/Tail/tail.cpp
@@ -27,7 +27,7 @@ public:
 	{
 		return n;
 	}
-	Integer operator+(Integer& other) const
+	Integer operator+(const Integer& other) const
 	{
 		Integer ans;
 		int top = (int)std::max(other.n, n);
@@ -44,7 +44,7 @@ public:
 		}
 		return ans;
 	}
-	Integer operator*(Integer& other) const
+	Integer operator*(const Integer& other) const
 	{
 		Integer ans;
 		ans.n = other.n + n;
@@ -66,6 +66,124 @@ public:
 			ans.n -= 1;
 		}
 		return ans;
+	}
+	bool operator<(const Integer& other) const
+	{
+		if (n < other.n)
+		{
+			return true;
+		}
+		if (n > other.n)
+		{
+			return false;
+		}
+		for (int i = (int)n; i >= 0; i--)
+		{
+			if (arr[i] < other.arr[i])
+			{
+				return true;
+			}
+			if (arr[i] > other.arr[i])
+			{
+				return false;
+			}
+		}
+		return false;
+	}
+	Integer operator%(const Integer& other) const
+	{
+		if (*this < other)
+		{
+			return *this;
+		}
+		Integer ans;
+
+		return ans;
+	}
+	Integer& operator=(long long other)
+	{
+		n = 1;
+		for (unsigned int& i : arr)
+		{
+			i = other % bitMod;
+			other /= bitMod;
+			if (other != 0)
+			{
+				n += 1;
+			}
+		}
+		return *this;
+	}
+	Integer& operator=(const Integer& other)
+	{
+		if (this == &other)
+		{
+			return *this;
+		}
+		n = other.n;
+		for (int i = 0; i < maxSize; i++)
+		{
+			arr[i] = other.arr[i];
+		}
+		return *this;
+	}
+	Integer& operator*=(long long other)
+	{
+		n += 5;
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] *= other;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			arr[i + 1] += arr[i] / bitMod;
+			arr[i] %= bitMod;
+		}
+		while (arr[n - 1] == 0 && n > 1)
+		{
+			n -= 1;
+		}
+		return *this;
+	}
+	Integer& operator+=(const Integer& other)
+	{
+		int top = (int)std::max(other.n, n);
+		n = top + 1;
+		for (int i = 0; i < top; i++)
+		{
+			arr[i] += other.arr[i];
+			arr[i + 1] += arr[i] / bitMod;
+			arr[i] %= bitMod;
+		}
+		while (arr[n - 1] == 0 && n > 1)
+		{
+			n -= 1;
+		}
+		return *this;
+	}
+	Integer& operator*=(const Integer& other)
+	{
+		Integer ans;
+		ans.n = other.n + n;
+		for (auto& i : ans.arr)
+		{
+			i = 0;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < other.n; j++)
+			{
+				ans.arr[i + j] += arr[i] * other.arr[j];
+				ans.arr[i + j + 1] += ans.arr[i + j] / bitMod;
+				ans.arr[i + j] %= bitMod;
+			}
+		}
+		while (ans.arr[ans.n - 1] == 0 && ans.n > 1)
+		{
+			ans.n -= 1;
+		}
+		*this = ans;
+		return *this;
 	}
 	friend std::istream& operator>>(std::istream& in, Integer& x)
 	{
@@ -100,6 +218,20 @@ public:
 		}
 		return out;
 	}
+	static Integer Pow(Integer a, unsigned int b)
+	{
+		Integer ans(1);
+		while (b != 0)
+		{
+			if (b & 1)
+			{
+				ans *= a;
+			}
+			a *= a;
+			b >>= 1;
+		}
+		return ans;
+	}
 };
 
 class Solution
@@ -109,6 +241,7 @@ private:
 	int cnt;
 	int* r;
 	int* p;
+	Integer* m;
 public:
 	explicit Solution(int kk)
 	{
@@ -116,11 +249,13 @@ public:
 		cnt = 0;
 		r = new int[k];
 		p = new int[k];
+		m = new Integer[k];
 	}
 	~Solution()
 	{
 		delete[] r;
 		delete[] p;
+		delete[] m;
 	}
 
 	void AddPair(int rr, int pp)
@@ -132,13 +267,33 @@ public:
 
 	int Solve()
 	{
-		return r[0];
+		for (int i = 0; i < k; i++)
+		{
+			m[i] = 1;
+			for (int j = 0; j < k; j++)
+			{
+				if (i != j)
+				{
+					m[i] *= p[i];
+				}
+			}
+			#ifdef DEBUG
+			std::cout << "/*****************/" << std::endl;
+			std::cout << "m[" << i << "] = " << m[i] << std::endl;
+			std::cout << "/*****************/" << std::endl;
+			#endif
+		}
+		Integer ans(0);
+		for (int i = 0; i < k; i++)
+		{
+			ans += Integer::Pow(m[i], p[i] - 2);
+		}
 	}
 };
 
 int main()
 {
-	//#ifdef BIGINTDEBUG
+	#ifdef BIGINTDEBUG
 	Integer x, y;
 	std::cin >> x >> y;
 	std::cout << x << std::endl << y;
@@ -147,7 +302,7 @@ int main()
 	Integer w = x * y;
 	std::cout << w << std::endl;
 	return 0;
-	//#endif
+	#endif
 	int k, r, p;
 	std::cin >> k;
 	Solution solution(k);
